befd6b5f5dacfea32efa02063298c7c7c5840550
dmcelectrico <dmcelectrico@e82ad366-f2da-f5e1-767d-be8b1eb56973>
2010-11-25 14:04:06 +0000

    Primera subida.

---
+++ Otosal/src/otosal/OtosalApp.java
@@ -0,0 +1,44 @@
+/*
+ * OtosalApp.java
+ */
+
+package otosal;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class OtosalApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new OtosalView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of OtosalApp
+     */
+    public static OtosalApp getApplication() {
+        return Application.getInstance(OtosalApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(OtosalApp.class, args);
+    }
+}
